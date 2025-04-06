dafd9077977bb5ec2ce745a1b3be8af5ee9b6cf8
Jonnathan Andres Sandino A <jasandinoa@gmail.com>
2025-04-05 23:42:11 -0500

    se agrega respuesta al servicio

--- coordinador/src/coordinador.service.ts
+++ coordinador/src/coordinador.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { ConsultaResponse } from './consulta.response.model';
 import { ConsultaRequest } from './consulta.request.model';
+import { ConsultaError } from './request.error.model';
 
 @Injectable()
 export class CoordinadorService {
-  consultas(consultaRequest: ConsultaRequest): ConsultaResponse {
+  consultas(
+    consultaRequest: ConsultaRequest,
+  ): ConsultaResponse | ConsultaError {
+    if (consultaRequest === undefined) {
+      return {
+        message: 'No se piuede procesar, el cuerpo del request es requerido',
+      };
+    }
     return {
-      id: consultaRequest.id,
-      message: 'Consulta realizada con éxito',
+      id_detalle_pedido: 1,
+      id_pedido: 1,
+      id_inventario: 1,
+      cantidad: 1,
     };
   }
 }
